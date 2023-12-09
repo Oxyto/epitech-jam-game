b0663117d561894355e24fabacdb720dd87149a7
Louis De Macedo <louis.de.macedo.ldm@gmail.com>
2023-12-09 11:08:42 +0100

    feat: add sound to menu

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,6 +2,8 @@ extends Control
 
 const PLAY_SCENE = ""
 
+@onready var ButtonAudio = $"Buttons"
+
 func _on_play():
 	get_tree().change_scene_to_file(PLAY_SCENE)
 
@@ -9,4 +11,4 @@ func _on_quit():
 	get_tree().quit()
 
 func _on_button_mouse_entered():
-	pass
+	ButtonAudio.play()
