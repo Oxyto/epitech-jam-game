da6420a7abd863d558308ed59070cd09f798f567
parosa47 <91087118+parosa47@users.noreply.github.com>
2023-12-09 22:21:31 +0100

    maze1

---
+++ Scripts/player_maze.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+var startingIntensity = 5.0
+var currentIntensity = startingIntensity
+var decreaseRate = 0.1
+
+func _physics_process(delta):
+	
+	currentIntensity -= decreaseRate * delta
+	currentIntensity = max(0, currentIntensity)
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionx = Input.get_axis("ui_left", "ui_right")
+	var directiony = Input.get_axis("ui_up", "ui_down")
+	if directionx:
+		velocity.x = directionx * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if directiony:
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
